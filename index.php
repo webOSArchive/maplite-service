2cd2603d34912c31c9fcaa9cc6d65f133c4731a7
codepoet <info@codepoetry.app>
2023-12-02 21:22:06 -0500

    update index to new template

--- index.php
+++ index.php
@@ -1,67 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
 <?php
-$config = include('config.php');
-include ("common.php");
-$bingKey = $config['bingAPIKey'];
-$ipinfoKey = $config['ipinfoKey'];
-$mapType = $config['defaultMapType'];
-$mapSize = $config['defaultMapSize'];
-$zoomLevel = $config['defaultZoomLevel'];
+  $config = include('config.php');
+  include ("common.php");
+  $bingKey = $config['bingAPIKey'];
+  $ipinfoKey = $config['ipinfoKey'];
+  $mapType = $config['defaultMapType'];
+  $mapSize = $config['defaultMapSize'];
+  $zoomLevel = $config['defaultZoomLevel'];
 ?>
-<html>  
-  <head>  
-    <title>webOS Maps</title>
-    <link rel="shortcut icon" sizes="256x256" href="icon-256.png">
-    <link rel="shortcut icon" sizes="196x196" href="icon-196.png">
-    <link rel="shortcut icon" sizes="128x128" href="icon-128.png">
-    <link rel="shortcut icon" href="favicon.ico">
-    <link rel="icon" type="image/png" href="icon.png" />
-    <link rel="apple-touch-icon" href="icon.png"/>
-    <link rel="apple-touch-startup-image" href="icon-256.png">
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="white" />
-    
-    <link rel="stylesheet" href="style.css">
-    <script>
-      function findScreenSize() {
-        document.getElementById("screenSize").value = window.innerWidth;
-      }
-    </script>
-  </head>  
-  <body onload="findScreenSize()">  
-    <?php
-    //Figure out what protocol the client wanted
-    if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
-      echo file_get_contents("https://www.webosarchive.org/menu.php?content=maps");
-      $REQUEST_PROTOCOL = "https";
-    }
-    else {
-      echo file_get_contents("http://www.webosarchive.org/menu.php?content=maps");
-      $REQUEST_PROTOCOL = "http";
-    }
-    $useLoc = geolocateByIP(getVisitorIP($config['hostname']), $ipinfoKey);
+<?php
+  //App Details
+  $title = "webOS Maps";
+  $subtitle = "";
+  $description = "Simplified maps for legacy devices.";
+  $github = "https://github.com/webosarchive/maplite-service";
+  $museumLink = "https://appcatalog.webosarchive.org/app/MapLite";
+  $icon = "icon.png";
 
-    if (isset($_POST['query'])) {
-      $useLoc = $_POST['query'];
-    }
+  //Figure out what protocol the client wanted
+  if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+    $PROTOCOL = "https";
+  } else {
+    $PROTOCOL = "http";
+  }
 
-    if (isset($_POST['zoom'])) {
-      $zoomLevel = $_POST['zoom'];
-    }
+  //Figure out map query
+  $useLoc = geolocateByIP(getVisitorIP($config['hostname']), $ipinfoKey);
+  if (isset($_POST['query'])) {
+    $useLoc = $_POST['query'];
+  }
 
-    if (isset($_POST['screenSize']) && $_POST['screenSize'] != "") {
-      $mapSize = round((int)$_POST['screenSize'] * 0.7);
-      $mapSize = $mapSize . "," . $mapSize;
-    }
-    ?>
-    <div class="content">
-      <p align='middle' style='margin-top:50px;'>
-        <a href='../'>
-        <img src='icon-128.png' style="width:128px; height: 128px;" border="0"></a><br/>
-        <strong>Map Lite</strong><br/>
-        <small>A project of <a href="<?php echo $REQUEST_PROTOCOL; ?>://www.webosarchive.org">webOS Archive</a></small><br>
-    <br/>
+  if (isset($_POST['zoom'])) {
+    $zoomLevel = $_POST['zoom'];
+  }
 
-      </p>
+  if (isset($_POST['screenSize']) && $_POST['screenSize'] != "") {
+    $mapSize = round((int)$_POST['screenSize'] * 0.7);
+    $mapSize = $mapSize . "," . $mapSize;
+  }
+?>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+
+  <meta name="description" content="<?php echo $description; ?>">
+  <meta name="keywords" content="webos, firefoxos, pwa, rss">
+  <meta name="author" content="webOS Archive">
+  <meta property="og:title" content="<?php echo $title; ?>">
+  <meta property="og:description" content="<?php echo $description; ?>">
+  <meta property="og:image" content="https://<?php echo $_SERVER['SERVER_NAME'] ?>/tracker/hero.png">
+
+  <meta name="twitter:card" content="app">
+  <meta name="twitter:site" content="@webOSArchive">
+  <meta name="twitter:title" content="<?php echo $title; ?>">
+  <meta name="twitter:description" content="<?php echo $description; ?>">
+  <meta name="twitter:app:id:googleplay" content="<?php echo $playId?>">
+
+  <title><?php echo $title . $subtitle; ?></title>
+  <script>
+    function findScreenSize() {
+      document.getElementById("screenSize").value = window.innerWidth;
+    }
+  </script>
+  <link id="favicon" rel="icon" type="image/png" sizes="64x64" href="<?php echo $icon;?>">
+  <link href="<?php echo $PROTOCOL . "://www.webosarchive.org/app-template/"?>web.css" rel="stylesheet" type="text/css" >
+</head>
+<body>
+<?php
+
+$docRoot = "./";
+echo file_get_contents("https://www.webosarchive.org/menu.php?docRoot=" . $docRoot . "&protocol=" . $PROTOCOL);
+?>
+
+  <table width="100%" border=0 style="width:100%;border:0px"><tr><td align="center" style="width:100%;height:100%;border:0px">
+  <div id="row">
+    <div id="content" align="left">
+      <h1><img src="<?php echo $icon;?>" width="60" height="60" alt=""/><?php echo $title; ?></h1>
+      <p><?php echo $description; ?></p>
+      <p>A Project of webOS Archive. Location provided by <a href='https://ipinfo.io'>IPInfo</a>, Maps provided by <a href='https://docs.microsoft.com/en-us/bingmaps/articles/accessing-the-bing-maps-rest-services-using-php'>Bing</a>.
+      <p>Host your own maps, or contribute on <a href="<?php echo $github;?>">GitHub.</a></p>
       <form method="post">
         <table border="0" cellpadding="0" cellspacing="0" class="content" style="margin: 0 auto;">
             <tr><td>Address: </td><td><input type="text" style="width:200px" name="query" value="<?php echo $useLoc ?>"></td></tr>
@@ -94,33 +113,43 @@ $zoomLevel = $config['defaultZoomLevel'];
           <tr><td colspan="3" align="center"><input type="submit" value="Update Map"></td></tr>
         </table> 
         <input type="hidden" name="screenSize" id="screenSize" style="display:none"/> 
-    </form>  
-    
-  <?php  
-    
-  if(isset($useLoc))  
-    {  
-      if (isset($_POST['key']) && $_POST['key'] != "") {
-        $bingKey = $_POST['key'];
-      }
-
-      if (isset($_POST['maptype'])) {
-        $mapType = $_POST['maptype'];
-      }
-
-      if (isset($_POST['mapsize'])) {
-        $mapSize = $_POST['mapsize'];
-      }
-
-      $mapInfo = getDataForLocation($useLoc, $mapType, $mapSize, ";36", $zoomLevel, $bingKey);
-      echo "<p align='middle'><img src='" . $mapInfo->img . "' style='margin: 0 auto; border-radius:2%; -webkit-border-radius:10px '></p>";
-      echo "<!--";
-      print_r($mapInfo);
-      echo "-->";
-  }  
-  ?>  
-
-<p align='middle' style="margin-top: 28px"><small>Location provided by <a href='https://ipinfo.io'>IPInfo</a>, Maps provided by <a href='https://docs.microsoft.com/en-us/bingmaps/articles/accessing-the-bing-maps-rest-services-using-php'>Bing</a> | <a href="https://github.com/webosarchive/maplite-service">Host this yourself</a> | <a href='<?php echo $REQUEST_PROTOCOL; ?>://appcatalog.webosarchive.org/app/maplite'>Download the webOS App</a></small></p>
+    </form> 
+      <p class="center">
+        <?php if (isset($museumLink)) { ?>
+        <a class="download-link" href="<?php echo $museumLink; ?>">
+          <img src="<?php echo $PROTOCOL . "://www.webosarchive.org/app-template/"?>museum-badge.png" width="200" height="59" alt="Get it on Google play" />
+        </a>
+        <?php } ?>
+      </p>
+    </div>
+    <div id="hero">
+    <?php  
+    if(isset($useLoc))  
+      {  
+        if (isset($_POST['key']) && $_POST['key'] != "") {
+          $bingKey = $_POST['key'];
+        }
+  
+        if (isset($_POST['maptype'])) {
+          $mapType = $_POST['maptype'];
+        }
+  
+        if (isset($_POST['mapsize'])) {
+          $mapSize = $_POST['mapsize'];
+        }
+  
+        $mapInfo = getDataForLocation($useLoc, $mapType, $mapSize, ";36", $zoomLevel, $bingKey);
+        echo "<p align='middle'><img src='" . $mapInfo->img . "' style='margin: 0 auto; border-radius:2%; -webkit-border-radius:10px '></p>";
+        echo "<!--";
+        print_r($mapInfo);
+        echo "-->";
+    }  
+    ?> 
+    </div>
   </div>
-</body>  
+  <div id="footer">
+    &copy;  webOSArchive
+  </div>
+  </td></tr></table>
+</body>
 </html>
